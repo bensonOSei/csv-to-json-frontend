a408e66fd84fe1a126970ee72d11d909a278c244
Benson Osei Mensah <kbenson643@gmail.com>
2024-08-28 14:22:22 +0000

    chore: add react-icons and tailwind forms plugin
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <div className="w-full max-w-lg mx-auto mt-10 p-2 bg-slate-50 rounded-md">
           <form>
             <label 
-            className="w-full grid place-items-center"
+            className="w-full grid place-items-center border border-dashed p-3 rounded-md hover:border-blue-500/50 hover:bg-blue-300/20 transition-all cursor-pointer"
             htmlFor="csv">
               test
             </label>
