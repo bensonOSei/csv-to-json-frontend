b2db0e8b943c8bb2e1e52e5a20b825470378f7f3
Benson Osei Mensah <kbenson643@gmail.com>
2024-08-28 14:17:27 +0000

    Initial commit
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+  return (
+    <>
+      <main className="bg-slate-100 p-4 h-screen">
+        <div className="w-full max-w-lg mx-auto mt-10 p-2 bg-slate-50 rounded-md">
+          <form>
+            <label 
+            className="w-full grid place-items-center"
+            htmlFor="csv">
+              test
+            </label>
+          </form>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
