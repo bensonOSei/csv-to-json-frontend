3e5f31fe4ce41340abddde2f0bb1d12f08724ff3
Benson Osei Mensah <kbenson643@gmail.com>
2024-08-31 08:41:03 +0000

    update uploader and implement json copy
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

---
+++ src/components/JsonReader.tsx
@@ -0,0 +1,73 @@
+import { FC, useState } from "react";
+import { FaRegCopy } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { TbCopyCheck } from "react-icons/tb";
+
+interface JsonReaderProps {
+  jsonString: string;
+}
+
+export const JsonReader: FC<JsonReaderProps> = ({ jsonString }) => {
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text);
+    handleCopied();
+  };
+  const beautifyJson = (json: string) => {
+    const data = JSON.parse(json);
+    const formattedJson = JSON.stringify(data, null, 2);
+    return formattedJson;
+  };
+  const [copied, setCopied] = useState<boolean>(false);
+  const [downloaded, setDownloaded] = useState<boolean>(false);
+
+  const handleCopied = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  const handleDownloaded = () => {
+    setDownloaded(true);
+    setTimeout(() => {
+      setDownloaded(false);
+    }, 2000);
+  };
+
+  const downloadJson = () => {
+    handleDownloaded();
+  }
+
+
+  return (
+    <div>
+      <h4 className="mb-2 text-lg font-bold text-blue-900">Results</h4>
+      <div className="h-80 overflow-hidden p-2 rounded-lg text-xs bg-slate-50 relative">
+        <pre>{beautifyJson(jsonString)}</pre>
+        <div className="absolute bg-gradient-to-b from-transparent from-10% to-slate-50 w-full h-full top-0 left-0 z-10"></div>
+      </div>
+      <div className="mt-3 flex items-center gap-2 p-1">
+        <button
+          onClick={() => copyToClipboard(jsonString)}
+          disabled={copied}
+          className="flex items-center text-xs p-1 px-2 bg-slate-100 rounded-md hover:bg-slate-200/70 transition-all hover:text-blue-800 text-blue-500/30 disabled:bg-green-500 disabled:text-stone-50"
+          type="button">
+          <span>
+            {copied ? "Copied!" : "Copy"}
+          </span>
+          {copied ? (
+            <TbCopyCheck className="ml-1" />
+          ) : (
+            <FaRegCopy className="ml-1" />
+          )}
+        </button>
+        <button
+          className="flex items-center text-xs p-1 px-2 bg-slate-100 rounded-md hover:bg-slate-200/70 transition-all hover:text-blue-800 text-blue-500/30"
+          type="button">
+          <span>Download JSON</span>
+          <LuDownload className="ml-1" />
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -1,14 +1,22 @@
 import { FC } from "react";
 
+type ButtonType = "button" | "submit" | "reset";
+
+
 interface PrimaryButtonProps {
   children: React.ReactNode;
+  onClick?: () => void;
+  type?: ButtonType;
+  disabled?: boolean
 }
 
-export const PrimaryButton: FC<PrimaryButtonProps> = ({ children }) => {
+export const PrimaryButton: FC<PrimaryButtonProps> = ({ children, onClick, type = 'button', disabled = false }) => {
   return (
     <button
-      type="button"
-      className="px-3 py-1 text-center bg-blue-400 text-slate-50 rounded-md hover:bg-blue-500 transition-all text-sm flex items-center gap-1">
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className="px-3 py-1 text-center bg-blue-400 text-slate-50 rounded-md hover:bg-blue-500 transition-all text-sm flex items-center gap-1 disabled:cursor-not-allowed disabled:opacity-30">
       {children}
     </button>
   );
